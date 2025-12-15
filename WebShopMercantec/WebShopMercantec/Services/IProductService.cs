47d8fda1f1d436bd4ac31934f14afc4e868085be
DeusFlow <deuswork@icloud.com>
2025-12-15 09:08:17 +0100

    Refactor IProductService interface comments for clarity and consistency

--- WebShopMercantec/WebShopMercantec/Services/IProductService.cs
+++ WebShopMercantec/WebShopMercantec/Services/IProductService.cs
@@ -2,10 +2,10 @@ using WebShopMercantec.Shared.DTOs;
 
 namespace WebShopMercantec.Services
 {
-    // Интерфейс всегда начинается с буквы "I" (Convention)
+    
     public interface IProductService
     {
-        // Task<...> — это обещание (Promise). Означает "Я верну результат, но чуть позже, асинхронно".
+        
         // IEnumerable — это просто список, по которому можно пройтись (перечисление).
         Task<IEnumerable<ProductDto>> GetAvailableProductsAsync();
         
